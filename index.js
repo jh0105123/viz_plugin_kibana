c1153b38764c59f0a86b8d7e8927fde33047a39e
백지혜 <jihye.paik@mirero.co.kr>
2019-09-24 18:54:41 +0900

    v5 적용

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-export default function (kibana) {
+import exampleRoute from './server/routes/example';
 
+export default function (kibana) {
   return new kibana.Plugin({
+    require: ['elasticsearch'],
+
     uiExports: {
-      visTypes: [
-        'plugins/viz_pivot/vis'
-      ]
+      visTypes: [ 'plugins/pivot_table/vis' ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
     }
+
+
   });
-}
+};
