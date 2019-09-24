340e0c163f4f29e1272bfd1f2b8e548a16ff0122
백지혜 <jihye.paik@mirero.co.kr>
2019-09-24 15:37:32 +0900

    v7 test_vis update

--- public/test_vis.js
+++ public/test_vis.js
@@ -5,13 +5,11 @@ import { VisController } from './vis_controller';
 
 import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
-import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
 
-function TestVisProvider(Private) {
+const MyNewVisType = (Private) => {
   const VisFactory = Private(VisFactoryProvider);
-  const Schemas = Private(VisSchemasProvider);
 
-  return VisFactory.createBaseVisualization({
+  return VisFactory.createAngularVisualization({
     name: 'test_vis',
     title: 'Test Vis',
     icon: 'fa fa-gear',
@@ -48,5 +46,48 @@ function TestVisProvider(Private) {
   });
 }
 
+VisTypesRegistryProvider.register(MyNewVisType);
+
+// function TestVisProvider(Private) {
+//   const VisFactory = Private(VisFactoryProvider);
+//   const Schemas = Private(VisSchemasProvider);
+
+//   return VisFactory.createBaseVisualization({
+//     name: 'test_vis',
+//     title: 'Test Vis',
+//     icon: 'fa fa-gear',
+//     description: 'test vuis',
+//     visualization: VisController,
+//     visConfig: {
+//       defaults: {
+//         // add default parameters
+//         fontSize: '30'
+//       },
+//     },
+//     editorConfig: {
+//       optionsTemplate: optionsTemplate,
+//       schemas: new Schemas([
+//         {
+//           group: 'metrics',
+//           name: 'metric',
+//           title: 'Metric',
+//           min: 1,
+//           aggFilter: ['!derivative', '!geo_centroid'],
+//           defaults: [
+//             { type: 'count', schema: 'metric' }
+//           ]
+//         }, {
+//           group: 'buckets',
+//           name: 'segment',
+//           title: 'Bucket Split',
+//           min: 0,
+//           max: 1,
+//           aggFilter: ['!geohash_grid', '!filter']
+//         }
+//       ]),
+//     }
+//   });
+// }
+
 // register the provider with the visTypes registry
-VisTypesRegistryProvider.register(TestVisProvider);
+// VisTypesRegistryProvider.register(TestVisProvider);
