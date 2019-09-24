32a0445bf78c9f66b9042231da6092920700b9ae
백지혜 <jihye.paik@mirero.co.kr>
2019-09-24 16:03:28 +0900

    test_viz 추가

--- public/test_vis.js
+++ public/test_vis.js
@@ -5,11 +5,12 @@ import { VisController } from './vis_controller';
 
 import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { Schemas } from 'ui/vis/editors/default/schemas';
 
-const MyNewVisType = (Private) => {
+function TestVisProvider(Private) {
   const VisFactory = Private(VisFactoryProvider);
 
-  return VisFactory.createAngularVisualization({
+  return VisFactory.createBaseVisualization({
     name: 'test_vis',
     title: 'Test Vis',
     icon: 'fa fa-gear',
@@ -22,72 +23,31 @@ const MyNewVisType = (Private) => {
       },
     },
     editorConfig: {
-      optionsTemplate: optionsTemplate
-      // schemas: new Schemas([
-      //   {
-      //     group: 'metrics',
-      //     name: 'metric',
-      //     title: 'Metric',
-      //     min: 1,
-      //     aggFilter: ['!derivative', '!geo_centroid'],
-      //     defaults: [
-      //       { type: 'count', schema: 'metric' }
-      //     ]
-      //   }, {
-      //     group: 'buckets',
-      //     name: 'segment',
-      //     title: 'Bucket Split',
-      //     min: 0,
-      //     max: 1,
-      //     aggFilter: ['!geohash_grid', '!filter']
-      //   }
-      // ]),
+      optionsTemplate: optionsTemplate,
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Metric',
+          min: 1,
+          aggFilter: ['!derivative', '!geo_centroid'],
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+        }, {
+          group: 'buckets',
+          name: 'segment',
+          title: 'Bucket Split',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
+        }
+      ]),
     }
   });
 }
 
-VisTypesRegistryProvider.register(MyNewVisType);
-
-// function TestVisProvider(Private) {
-//   const VisFactory = Private(VisFactoryProvider);
-//   const Schemas = Private(VisSchemasProvider);
-
-//   return VisFactory.createBaseVisualization({
-//     name: 'test_vis',
-//     title: 'Test Vis',
-//     icon: 'fa fa-gear',
-//     description: 'test vuis',
-//     visualization: VisController,
-//     visConfig: {
-//       defaults: {
-//         // add default parameters
-//         fontSize: '30'
-//       },
-//     },
-//     editorConfig: {
-//       optionsTemplate: optionsTemplate,
-//       schemas: new Schemas([
-//         {
-//           group: 'metrics',
-//           name: 'metric',
-//           title: 'Metric',
-//           min: 1,
-//           aggFilter: ['!derivative', '!geo_centroid'],
-//           defaults: [
-//             { type: 'count', schema: 'metric' }
-//           ]
-//         }, {
-//           group: 'buckets',
-//           name: 'segment',
-//           title: 'Bucket Split',
-//           min: 0,
-//           max: 1,
-//           aggFilter: ['!geohash_grid', '!filter']
-//         }
-//       ]),
-//     }
-//   });
-// }
-
 // register the provider with the visTypes registry
-// VisTypesRegistryProvider.register(TestVisProvider);
+VisTypesRegistryProvider.register(TestVisProvider);
+
+export default TestVisProvider;
