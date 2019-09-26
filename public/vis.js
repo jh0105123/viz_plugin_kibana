82cbd89e7662b05fa7d3c57c0f5677cfe15634c5
백지혜 <jihye.paik@mirero.co.kr>
2019-09-27 08:20:00 +0900

    schemas 제거

--- public/vis.js
+++ public/vis.js
@@ -31,26 +31,26 @@ const MyNewVisType = (Private) => {
       //   { title: 'tab 1', template: '<div>test</div>' },
       //   { title: 'tab 2', template: optionsTemplate }
       // ],
-      schemas: new Schemas([
-        // {
-        //   group: 'metrics',
-        //   name: 'metric',
-        //   title: 'Metric',
-        //   min: 1,
-        //   aggFilter: ['!derivative', '!geo_centroid'],
-        //   defaults: [
-        //     { type: 'count', schema: 'metric' }
-        //   ]
-        // }, 
-        // {
-        //   group: 'buckets',
-        //   name: 'segment',
-        //   title: 'Bucket Split',
-        //   min: 0,
-        //   max: 1,
-        //   aggFilter: ['!geohash_grid', '!filter']
-        // }
-      ])
+      // schemas: new Schemas([
+      //   // {
+      //   //   group: 'metrics',
+      //   //   name: 'metric',
+      //   //   title: 'Metric',
+      //   //   min: 1,
+      //   //   aggFilter: ['!derivative', '!geo_centroid'],
+      //   //   defaults: [
+      //   //     { type: 'count', schema: 'metric' }
+      //   //   ]
+      //   // }, 
+      //   // {
+      //   //   group: 'buckets',
+      //   //   name: 'segment',
+      //   //   title: 'Bucket Split',
+      //   //   min: 0,
+      //   //   max: 1,
+      //   //   aggFilter: ['!geohash_grid', '!filter']
+      //   // }
+      // ])
     },
     options: { showIndexSelection: false }
   });
