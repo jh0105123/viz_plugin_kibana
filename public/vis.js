27250e92aabf09e627a0a06a6ec2d5f98000373f
백지혜 <jihye.paik@mirero.co.kr>
2019-09-26 14:32:35 +0900

    editorConfig 주석

--- public/vis.js
+++ public/vis.js
@@ -23,26 +23,26 @@ function TestVisProvider(Private) {
       },
     },
     editorConfig: {
-      optionsTemplate: optionsTemplate,
-      schemas: new Schemas([
-        {
-          group: 'metrics',
-          name: 'metric',
-          title: 'Metric',
-          min: 1,
-          aggFilter: ['!derivative', '!geo_centroid'],
-          defaults: [
-            { type: 'count', schema: 'metric' }
-          ]
-        }, {
-          group: 'buckets',
-          name: 'segment',
-          title: 'Bucket Split',
-          min: 0,
-          max: 1,
-          aggFilter: ['!geohash_grid', '!filter']
-        }
-      ]),
+      // optionsTemplate: optionsTemplate,
+      // schemas: new Schemas([
+      //   {
+      //     group: 'metrics',
+      //     name: 'metric',
+      //     title: 'Metric',
+      //     min: 1,
+      //     aggFilter: ['!derivative', '!geo_centroid'],
+      //     defaults: [
+      //       { type: 'count', schema: 'metric' }
+      //     ]
+      //   }, {
+      //     group: 'buckets',
+      //     name: 'segment',
+      //     title: 'Bucket Split',
+      //     min: 0,
+      //     max: 1,
+      //     aggFilter: ['!geohash_grid', '!filter']
+      //   }
+      // ]),
     },
     options:{
       showIndexSelection:false
