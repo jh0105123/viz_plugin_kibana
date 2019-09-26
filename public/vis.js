496db2e0a351bc11ec684bfbc664ec521c1e3b5b
백지혜 <jihye.paik@mirero.co.kr>
2019-09-27 08:38:49 +0900

    editor 추가

--- public/vis.js
+++ public/vis.js
@@ -23,37 +23,36 @@ const MyNewVisType = (Private) => {
         fontSize: '30'
       }
     },
-    // editor: 'default',
-    editorConfig: {
-      //optionsTemplate: '<div>test</div>',
-      optionsTemplate: optionsTemplate,
-      // optionsTabs: [
-      //   { title: 'tab 1', template: '<div>test</div>' },
-      //   { title: 'tab 2', template: optionsTemplate }
-      // ],
-      // schemas: new Schemas([
-      //   // {
-      //   //   group: 'metrics',
-      //   //   name: 'metric',
-      //   //   title: 'Metric',
-      //   //   min: 1,
-      //   //   aggFilter: ['!derivative', '!geo_centroid'],
-      //   //   defaults: [
-      //   //     { type: 'count', schema: 'metric' }
-      //   //   ]
-      //   // }, 
-      //   // {
-      //   //   group: 'buckets',
-      //   //   name: 'segment',
-      //   //   title: 'Bucket Split',
-      //   //   min: 0,
-      //   //   max: 1,
-      //   //   aggFilter: ['!geohash_grid', '!filter']
-      //   // }
-      // ])
-    },
+    editor: optionsTemplate,
+    // editorConfig: {
+    //   //optionsTemplate: '<div>test</div>',
+    //   optionsTemplate: optionsTemplate,
+    //   // optionsTabs: [
+    //   //   { title: 'tab 1', template: '<div>test</div>' },
+    //   //   { title: 'tab 2', template: optionsTemplate }
+    //   // ],
+    //   // schemas: new Schemas([
+    //   //   // {
+    //   //   //   group: 'metrics',
+    //   //   //   name: 'metric',
+    //   //   //   title: 'Metric',
+    //   //   //   min: 1,
+    //   //   //   aggFilter: ['!derivative', '!geo_centroid'],
+    //   //   //   defaults: [
+    //   //   //     { type: 'count', schema: 'metric' }
+    //   //   //   ]
+    //   //   // }, 
+    //   //   // {
+    //   //   //   group: 'buckets',
+    //   //   //   name: 'segment',
+    //   //   //   title: 'Bucket Split',
+    //   //   //   min: 0,
+    //   //   //   max: 1,
+    //   //   //   aggFilter: ['!geohash_grid', '!filter']
+    //   //   // }
+    //   // ])
+    // },
     options: { showIndexSelection: false },
-    responseHandler: 'none'
   });
 }
 
