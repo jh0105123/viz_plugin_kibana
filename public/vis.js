ee8d22fba8c5c05861423c8a5c259732c0780b4c
백지혜 <jihye.paik@mirero.co.kr>
2019-09-26 15:35:31 +0900

    createAngularVisualization로 변경

--- public/vis.js
+++ public/vis.js
@@ -23,29 +23,29 @@ const MyNewVisType = (Private) => {
         fontSize: '30'
       },
     },
-    editor: EditorController,
+    // editor: EditorController,
     editorConfig: {
-      my:'custom config'
-      // optionsTemplate: optionsTemplate,
-      // schemas: new Schemas([
-      //   {
-      //     group: 'metrics',
-      //     name: 'metric',
-      //     title: 'Metric',
-      //     min: 1,
-      //     aggFilter: ['!derivative', '!geo_centroid'],
-      //     defaults: [
-      //       { type: 'count', schema: 'metric' }
-      //     ]
-      //   }, {
-      //     group: 'buckets',
-      //     name: 'segment',
-      //     title: 'Bucket Split',
-      //     min: 0,
-      //     max: 1,
-      //     aggFilter: ['!geohash_grid', '!filter']
-      //   }
-      // ]),
+      // my:'custom config'
+      optionsTemplate: optionsTemplate,
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Metric',
+          min: 1,
+          aggFilter: ['!derivative', '!geo_centroid'],
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+        }, {
+          group: 'buckets',
+          name: 'segment',
+          title: 'Bucket Split',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
+        }
+      ])
     }
   });
 }
