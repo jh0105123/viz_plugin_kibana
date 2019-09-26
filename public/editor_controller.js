f4ac2dbcdb5ed0cd364c74b6adfcc7fd2844547f
백지혜 <jihye.paik@mirero.co.kr>
2019-09-26 15:19:38 +0900

    editor custom 중

---
+++ public/editor_controller.js
@@ -0,0 +1,16 @@
+class EditorController {
+    constructor(el, vis) {
+      this.el = el;
+      this.vis = vis;
+      this.config = vis.type.editorConfig;
+   }
+   async render(visData) {
+      <b>${this.config.my}</b>
+      return 'done rendering';
+   }
+   destroy() {
+      console.log('destroying');
+   }
+}
+
+export { EditorController };
--- public/vis.js
+++ public/vis.js
@@ -1,7 +1,8 @@
 import './vis.less';
 
-import optionsTemplate from './options_template.html';
+import optionsTemplate from './options_template.js';
 import { VisController } from './vis_controller';
+import { EditorController } from './editor_controller';
 
 import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
@@ -15,35 +16,36 @@ function TestVisProvider(Private) {
     title: 'Pivot Table',
     icon: 'tableOfContents',
     description: 'Pivot Table',
-    // visualization: VisController,
-    editor: VisController,
-    editorConfig: {
+    visualization: VisController,
+    visConfig: {
       defaults: {
         // add default parameters
         fontSize: '30'
       },
     },
+    editor: EditorController,
     editorConfig: {
-      optionsTemplate: optionsTemplate,
-      schemas: new Schemas([
-        {
-          group: 'metrics',
-          name: 'metric',
-          title: 'Metric',
-          min: 1,
-          aggFilter: ['!derivative', '!geo_centroid'],
-          defaults: [
-            { type: 'count', schema: 'metric' }
-          ]
-        }, {
-          group: 'buckets',
-          name: 'segment',
-          title: 'Bucket Split',
-          min: 0,
-          max: 1,
-          aggFilter: ['!geohash_grid', '!filter']
-        }
-      ]),
+      my:'custom config'
+      // optionsTemplate: optionsTemplate,
+      // schemas: new Schemas([
+      //   {
+      //     group: 'metrics',
+      //     name: 'metric',
+      //     title: 'Metric',
+      //     min: 1,
+      //     aggFilter: ['!derivative', '!geo_centroid'],
+      //     defaults: [
+      //       { type: 'count', schema: 'metric' }
+      //     ]
+      //   }, {
+      //     group: 'buckets',
+      //     name: 'segment',
+      //     title: 'Bucket Split',
+      //     min: 0,
+      //     max: 1,
+      //     aggFilter: ['!geohash_grid', '!filter']
+      //   }
+      // ]),
     }
   });
 }
