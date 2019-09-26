4adfe3414b4793e744a0e6cc3fb9d21408bebf3b
백지혜 <jihye.paik@mirero.co.kr>
2019-09-26 14:34:18 +0900

    원상 복귀

--- public/vis.js
+++ public/vis.js
@@ -25,27 +25,24 @@ function TestVisProvider(Private) {
     editorConfig: {
       optionsTemplate: optionsTemplate,
       schemas: new Schemas([
-        // {
-        //   group: 'metrics',
-        //   name: 'metric',
-        //   title: 'Metric',
-        //   min: 1,
-        //   aggFilter: ['!derivative', '!geo_centroid'],
-        //   defaults: [
-        //     { type: 'count', schema: 'metric' }
-        //   ]
-        // }, {
-        //   group: 'buckets',
-        //   name: 'segment',
-        //   title: 'Bucket Split',
-        //   min: 0,
-        //   max: 1,
-        //   aggFilter: ['!geohash_grid', '!filter']
-        // }
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Metric',
+          min: 1,
+          aggFilter: ['!derivative', '!geo_centroid'],
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+        }, {
+          group: 'buckets',
+          name: 'segment',
+          title: 'Bucket Split',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
+        }
       ]),
-    },
-    options:{
-      showIndexSelection:false
     }
   });
 }
