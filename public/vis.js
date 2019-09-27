a523acb4cbded32bdbfa97b96f47929c91bd7cfb
백지혜 <jihye.paik@mirero.co.kr>
2019-09-27 09:10:21 +0900

    function 변수 지정

--- public/vis.js
+++ public/vis.js
@@ -7,7 +7,7 @@ import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { Schemas } from 'ui/vis/editors/default/schemas';
 
-function TestVisProvider(Private) {
+const MyNewVisType = (Private) => {
   const VisFactory = Private(VisFactoryProvider);
 
   return VisFactory.createBaseVisualization({
@@ -21,34 +21,33 @@ function TestVisProvider(Private) {
         // add default parameters
         fontSize: '30'
       },
+    },
+    editorConfig: {
+      optionsTemplate: optionsTemplate,
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Metric',
+          min: 1,
+          aggFilter: ['!derivative', '!geo_centroid'],
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+        }, {
+          group: 'buckets',
+          name: 'segment',
+          title: 'Bucket Split',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
+        }
+      ]),
     }
-    // ,
-    // editorConfig: {
-    //   optionsTemplate: optionsTemplate,
-    //   schemas: new Schemas([
-    //     {
-    //       group: 'metrics',
-    //       name: 'metric',
-    //       title: 'Metric',
-    //       min: 1,
-    //       aggFilter: ['!derivative', '!geo_centroid'],
-    //       defaults: [
-    //         { type: 'count', schema: 'metric' }
-    //       ]
-    //     }, {
-    //       group: 'buckets',
-    //       name: 'segment',
-    //       title: 'Bucket Split',
-    //       min: 0,
-    //       max: 1,
-    //       aggFilter: ['!geohash_grid', '!filter']
-    //     }
-    //   ]),
-    // }
   });
 }
 
 // register the provider with the visTypes registry
-VisTypesRegistryProvider.register(TestVisProvider);
+VisTypesRegistryProvider.register(MyNewVisType);
 
 //export default TestVisProvider;
