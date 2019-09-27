3d313116c21ac42357de8f6ea36aef63735591a9
백지혜 <jihye.paik@mirero.co.kr>
2019-09-27 13:49:08 +0900

    value alert

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -8,43 +8,6 @@ import customData from './data.json';
 window.$ = window.jQuery = jQuery;
 
 export class VisController{
-  // constructor(el, vis) {
-  //   this.vis = vis;
-  //   this.el = el;
-
-  //   this.container = document.createElement('div');
-  //   this.container.className = 'myvis-container-div';
-  //   this.el.appendChild(this.container);
-
-  //   $( document ).ready( function() {
-  //     $(".output").pivotUI(
-  //       customData,
-  //       {
-  //         rows: ["TEAM"],
-  //         vals: ["시간"],
-  //         aggregatorName: "Count"
-  //       }
-  //     );
-  //   } );
-
-  //   const pivotDiv = document.createElement('div');
-  //   pivotDiv.className = 'output';
-  //   this.container.appendChild(pivotDiv);
-  // }
-
-  // destroy() {
-  //   this.el.innerHTML = '';
-  // }
-
-  // render(visData, status) {
-  //   const config = this.vis.params.metric;
-
-  //   alert(config);
-
-  //   return new Promise(resolve => {
-  //     resolve('when done rendering');
-  //   });
-  // }
   constructor(el, vis) {
     this.vis = vis;
     this.el = el;
@@ -52,6 +15,21 @@ export class VisController{
     this.container = document.createElement('div');
     this.container.className = 'myvis-container-div';
     this.el.appendChild(this.container);
+
+    $( document ).ready( function() {
+      $(".output").pivotUI(
+        customData,
+        {
+          rows: ["TEAM"],
+          vals: ["시간"],
+          aggregatorName: "Count"
+        }
+      );
+    } );
+
+    const pivotDiv = document.createElement('div');
+    pivotDiv.className = 'output';
+    this.container.appendChild(pivotDiv);
   }
 
   destroy() {
@@ -59,49 +37,12 @@ export class VisController{
   }
 
   render(visData, status) {
-    this.container.innerHTML = '';
+    const value = visData.columns.name;
 
-    const table = visData;
-    const metrics = [];
-    let bucketAgg;
-
-    table.columns.forEach((column, i) => {
-      // we have multiple rows … first column is a bucket agg
-      if (table.rows.length > 1 && i == 0) {
-        bucketAgg = column.aggConfig;
-        return;
-      }
-
-      table.rows.forEach(row => {
-        const value = row[i];
-        metrics.push({
-          title: bucketAgg ? `${row[0]} ${column.title}` : column.title,
-          value: row[i],
-          formattedValue: column.aggConfig ? column.aggConfig.fieldFormatter('text')(value) : value,
-          bucketValue: bucketAgg ? row[0] : null,
-          aggConfig: column.aggConfig
-        });
-      });
-    });
-
-    metrics.forEach(metric => {
-      const metricDiv = document.createElement(`div`);
-      metricDiv.className = `myvis-metric-div`;
-      metricDiv.innerHTML = `<b>${metric.title}:</b> ${metric.formattedValue}`;
-      metricDiv.setAttribute('style', `font-size: ${this.vis.params.fontSize}pt`);
-      metricDiv.addEventListener('click', () => {
-        if (!bucketAgg) return;
-        const filter = bucketAgg.createFilter(metric.bucketValue);
-        this.vis.API.queryFilter.addFilters(filter);
-      });
-
-      this.container.appendChild(metricDiv);
-    });
+    alert(value);
 
     return new Promise(resolve => {
-
       resolve('when done rendering');
     });
   }
 };
-
