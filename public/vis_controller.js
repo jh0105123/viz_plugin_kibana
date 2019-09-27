fdef8035655fbb460785407e9c15df31d0f0ce95
백지혜 <jihye.paik@mirero.co.kr>
2019-09-27 18:16:44 +0900

    string to json

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -16,7 +16,7 @@ export class VisController{
     this.container.className = 'myvis-container-div';
     this.el.appendChild(this.container);
 
-    const data = vis.indexPattern.originalBody.fields;
+    const data = eval(vis.indexPattern.originalBody.fields);
     const pivotDiv = document.createElement('div');
     pivotDiv.className = 'output';
     this.container.appendChild(pivotDiv);
