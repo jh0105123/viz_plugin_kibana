1fedfda4bbe1bb9b4da1fe4da074cc94679d5a26
백지혜 <jihye.paik@mirero.co.kr>
2019-10-01 11:37:22 +0900

    getDate

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -34,7 +34,9 @@ export class VisController {
     // pivotDiv.className = "output";
     // this.container.appendChild(pivotDiv);
   }
-  _render(visData, status) {}
+  async _render(visData, status) {
+    const data = visData.getData();
+  }
   // 변화 감지 이벤트 받는 곳
   // 컴포넌스가 어떻게 생겼는지 정의하는 역할
   // html 형식의 문자열을 반환하지 않고 뷰가 어떻게 생겼고 어떻게 작동하는지에 대한 정보
@@ -46,9 +48,6 @@ export class VisController {
     // });
 
     return this._render(visData, status);
-    // return new Promise(resolve => {
-    //   resolve("when done rendering");
-    // });
   }
 
   destroy() {
