1f5b19a6700360b4322164c0cd5ddd2b7d75ac93
백지혜 <jihye.paik@mirero.co.kr>
2019-10-01 11:28:55 +0900

    ready 삭제

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -43,12 +43,10 @@ export class VisController {
   // 컴포넌스가 어떻게 생겼는지 정의하는 역할
   // html 형식의 문자열을 반환하지 않고 뷰가 어떻게 생겼고 어떻게 작동하는지에 대한 정보
   async render(visData, status) {
-    $(document).ready(function() {
-      $(".output").pivotUI(customData, {
-        // rows: ["TEAM"],
-        // vals: ["시간"],
-        // aggregatorName: "Sum"
-      });
+    $(".output").pivotUI(customData, {
+      // rows: ["TEAM"],
+      // vals: ["시간"],
+      // aggregatorName: "Sum"
     });
 
     return new Promise(resolve => {
