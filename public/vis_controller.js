00721c7eaac970bea0f9256e45cf481da264f149
백지혜 <jihye.paik@mirero.co.kr>
2019-10-08 17:02:16 +0900

    default 추가

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -75,6 +75,9 @@ export class VisController {
   }
 
   async renderPivotUITable(result, metericType, valsType) {
+    if(this.config.rendererName==undefined)
+    this.config.rendererName="table";
+
     $(".output").pivotUI(result, {
       // cols: $scope.table.config.cols,
       // rows: $scope.table.config.rows,
