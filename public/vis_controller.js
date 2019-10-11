5bee327956e31c423b8f1d40d99bb001963139dd
백지혜 <jihye.paik@mirero.co.kr>
2019-10-11 17:34:35 +0900

    configChange.call(); 추가

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -121,6 +121,7 @@ export class VisController {
           globals.rendererName = config.rendererName;
           globals.rows = config.rows;
           globals.cols = config.cols;
+          configChange.call();
         }
       },
       true
