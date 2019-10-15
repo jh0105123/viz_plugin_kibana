c9aec760d76ff3a3aa37930069412561b9f3b6f1
백지혜 <jihye.paik@mirero.co.kr>
2019-10-15 10:45:54 +0900

    다시 추가

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -70,8 +70,8 @@ export class VisController {
       this.renderPivotUITable(result, metericType, valsType);
     else this.renderPivotTable(result, metericType, valsType);
 
-    // this.vis.params.rows = visData.rows;
-    // this.vis.params.cols = visData.columns;
+    this.vis.params.rows = visData.rows;
+    this.vis.params.cols = visData.columns;
 
     return new Promise(resolve => {
       resolve("when done rendering");
