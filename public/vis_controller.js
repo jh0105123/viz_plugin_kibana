2651bc2810f6cb9aa0af43d80d11e24d44199aad
백지혜 <jihye.paik@mirero.co.kr>
2019-10-15 14:09:56 +0900

    this.vis.updateState();

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -137,17 +137,11 @@ export class VisController {
 
     const stateCopy = this.vis.getState();
 
-    if (
-      stateCopy.params.rendererName != globals.rendererName ||
-      stateCopy.params.rows != globals.rows ||
-      stateCopy.params.cols != globals.cols
-    ) {
-      stateCopy.params.rendererName = globals.rendererName;
-      stateCopy.params.rows = globals.rows;
-      stateCopy.params.cols = globals.cols;
-      this.vis.setState(stateCopy);
-      this.vis.updateState();
-    }
+    stateCopy.params.rendererName = globals.rendererName;
+    stateCopy.params.rows = globals.rows;
+    stateCopy.params.cols = globals.cols;
+    this.vis.setState(stateCopy);
+    this.vis.updateState();
   }
 
   destroy() {
