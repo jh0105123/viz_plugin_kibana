dd5818870ddfc1c453e477065658228482549b3e
백지혜 <jihye.paik@mirero.co.kr>
2019-10-15 10:57:09 +0900

    status.rows = globals.rows; 추가

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -70,9 +70,6 @@ export class VisController {
       this.renderPivotUITable(result, metericType, valsType);
     else this.renderPivotTable(result, metericType, valsType);
 
-    status.rows = globals.rows;
-    status.cols = globals.cols;
-
     return new Promise(resolve => {
       resolve("when done rendering");
     });
@@ -130,6 +127,8 @@ export class VisController {
     globals.rendererName = config.rendererName;
     globals.rows = config.rows;
     globals.cols = config.cols;
+
+    status.rows = globals.rows;
   }
 
   destroy() {
