ff50b08f55203b738fa0a3f7708eb41313c530a3
백지혜 <jihye.paik@mirero.co.kr>
2019-10-22 14:57:11 +0900

    수정중

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -6,21 +6,6 @@ import "pivottable/dist/pivottable.css";
 import "pivottable/dist/plotly_renderers.js";
 import "./css/custom.css";
 
-let globals = {
-  rendererName: "Table",
-  cols: [],
-  rows: [],
-  metericTypes: new Map([
-    ["Count", "Count"],
-    ["Average", "Average"],
-    ["Sum", "Sum"],
-    ["Min", "Minimum"],
-    ["Max", "Maximum"],
-    ["Median", "Median"],
-    ["Standard Deviation", "Sample Standard Deviation"]
-  ])
-};
-
 window.$ = window.jQuery = jQuery;
 export class VisController {
   constructor(el, vis) {
@@ -35,14 +20,27 @@ export class VisController {
     this.pivottableVis = document.createElement("div");
     this.pivottableVis.className = "output";
     this.container.appendChild(this.pivottableVis);
-
-    globals.rendererName = vis.params.rendererName;
-    globals.rows = vis.params.rows;
-    globals.cols = vis.params.cols;
   }
 
   async render(visData, status) {
-    this.$scope = $rootScope.$new();
+    let configs = {
+      rendererName: "Table",
+      cols: [],
+      rows: [],
+      metericTypes: new Map([
+        ["Count", "Count"],
+        ["Average", "Average"],
+        ["Sum", "Sum"],
+        ["Min", "Minimum"],
+        ["Max", "Maximum"],
+        ["Median", "Median"],
+        ["Standard Deviation", "Sample Standard Deviation"]
+      ])
+    };
+
+    configs.rendererName = vis.params.rendererName;
+    configs.rows = vis.params.rows;
+    configs.cols = vis.params.cols;
 
     var columnsName = [];
     var result = [];
@@ -51,13 +49,13 @@ export class VisController {
     var valsType = [];
 
     // status.dimensions.metric.forEach(metric => {
-    //   metericType = globals.metericTypes.get(metric.aggType);
+    //   metericType = configs.metericTypes.get(metric.aggType);
     // });
 
     visData.columns.forEach((column, index) => {
       if (index == visData.columns.length - 1) {
         valsType.push(column["name"]);
-        metericType = globals.metericTypes.get(column["name"].split(" ")[0]);
+        metericType = configs.metericTypes.get(column["name"].split(" ")[0]);
       }
       columnsName.push(column["name"]);
     });
@@ -71,24 +69,24 @@ export class VisController {
     });
 
     if (this.vis.params.editMode)
-      await this.renderPivotUITable(result, metericType, valsType);
-    else await this.renderPivotTable(result, metericType, valsType);
+      await this.renderPivotUITable(result, metericType, valsType, configs);
+    else await this.renderPivotTable(result, metericType, valsType, configs);
 
-    this.vis.params.rendererName = globals.rendererName;
-    this.vis.params.rows = globals.rows;
-    this.vis.params.cols = globals.cols;
+    this.vis.params.rendererName = configs.rendererName;
+    this.vis.params.rows = configs.rows;
+    this.vis.params.cols = configs.cols;
 
     return new Promise(resolve => {
       resolve("when done rendering");
     });
   }
 
-  async renderPivotTable(result, metericType, valsType) {
+  async renderPivotTable(result, metericType, valsType, configs) {
     $(".output").pivot(
       result,
       {
-        cols: globals.cols,
-        rows: globals.rows,
+        cols: configs.cols,
+        rows: configs.rows,
         vals: valsType,
         aggregatorName: metericType,
         aggregator: $.pivotUtilities.aggregators[metericType](valsType),
@@ -96,23 +94,23 @@ export class VisController {
           $.pivotUtilities.renderers,
           $.pivotUtilities.plotly_renderers
         ),
-        rendererName: globals.rendererName,
-        renderer: $.pivotUtilities.renderers[globals.rendererName],
+        rendererName: configs.rendererName,
+        renderer: $.pivotUtilities.renderers[configs.rendererName],
         onRefresh: await this.configChanged.bind(this)
       },
       true
     );
   }
 
-  async renderPivotUITable(result, metericType, valsType) {
+  async renderPivotUITable(result, metericType, valsType, configs) {
     $(".output").pivotUI(
       result,
       {
-        cols: globals.cols,
-        rows: globals.rows,
+        cols: configs.cols,
+        rows: configs.rows,
         vals: valsType,
         aggregatorName: metericType,
-        rendererName: globals.rendererName,
+        rendererName: configs.rendererName,
         renderers: $.extend(
           $.pivotUtilities.renderers,
           $.pivotUtilities.plotly_renderers
@@ -128,20 +126,20 @@ export class VisController {
 
     if (config.rendererName == null) config.rendererName = "Table";
 
-    globals.rendererName = config.rendererName;
-    globals.rows = config.rows;
-    globals.cols = config.cols;
+    configs.rendererName = config.rendererName;
+    configs.rows = config.rows;
+    configs.cols = config.cols;
 
     const stateCopy = this.vis.getState();
 
     if (
-      stateCopy.params.rendererName != globals.rendererName ||
-      JSON.stringify(stateCopy.params.rows) != JSON.stringify(globals.rows) ||
-      JSON.stringify(stateCopy.params.cols) != JSON.stringify(globals.cols)
+      stateCopy.params.rendererName != configs.rendererName ||
+      JSON.stringify(stateCopy.params.rows) != JSON.stringify(configs.rows) ||
+      JSON.stringify(stateCopy.params.cols) != JSON.stringify(configs.cols)
     ) {
-      stateCopy.params.rendererName = globals.rendererName;
-      stateCopy.params.rows = globals.rows;
-      stateCopy.params.cols = globals.cols;
+      stateCopy.params.rendererName = configs.rendererName;
+      stateCopy.params.rows = configs.rows;
+      stateCopy.params.cols = configs.cols;
       this.vis.setState(stateCopy);
     }
   }
