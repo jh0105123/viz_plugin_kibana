d4171da258fa138e17b3299ca9c24cd34565b4a1
백지혜 <jihye.paik@mirero.co.kr>
2019-10-22 15:04:17 +0900

    configChanged 주석

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -122,26 +122,22 @@ export class VisController {
   }
 
   configChanged(config) {
-    if (config == undefined) return;
-
-    if (config.rendererName == null) config.rendererName = "Table";
-
-    configs.rendererName = config.rendererName;
-    configs.rows = config.rows;
-    configs.cols = config.cols;
-
-    const stateCopy = this.vis.getState();
-
-    if (
-      stateCopy.params.rendererName != configs.rendererName ||
-      JSON.stringify(stateCopy.params.rows) != JSON.stringify(configs.rows) ||
-      JSON.stringify(stateCopy.params.cols) != JSON.stringify(configs.cols)
-    ) {
-      stateCopy.params.rendererName = configs.rendererName;
-      stateCopy.params.rows = configs.rows;
-      stateCopy.params.cols = configs.cols;
-      this.vis.setState(stateCopy);
-    }
+    // if (config == undefined) return;
+    // if (config.rendererName == null) config.rendererName = "Table";
+    // configs.rendererName = config.rendererName;
+    // configs.rows = config.rows;
+    // configs.cols = config.cols;
+    // const stateCopy = this.vis.getState();
+    // if (
+    //   stateCopy.params.rendererName != configs.rendererName ||
+    //   JSON.stringify(stateCopy.params.rows) != JSON.stringify(configs.rows) ||
+    //   JSON.stringify(stateCopy.params.cols) != JSON.stringify(configs.cols)
+    // ) {
+    //   stateCopy.params.rendererName = configs.rendererName;
+    //   stateCopy.params.rows = configs.rows;
+    //   stateCopy.params.cols = configs.cols;
+    //   this.vis.setState(stateCopy);
+    // }
   }
 
   destroy() {
